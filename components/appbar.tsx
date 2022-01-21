64fecd001ae4b5f9e37463c94da23f24836fea24
Firstbober <firstbober@tutanota.com>
2022-01-21 14:26:30 +0100

    Working top bar and sidebar. Add license.

---
+++ components/appbar.tsx
@@ -0,0 +1,69 @@
+/**
+ * Top screen bar with actions
+ *
+ * Appbar props define function for closing and opening sidebar, I think Redux will do good job here
+ * as Home component does not need to know about sidebar state.
+ */
+
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import IconButton from '@mui/material/IconButton';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Tooltip from "@mui/material/Tooltip"
+
+// Icons
+import MenuIcon from "@mui/icons-material/Menu"
+import CloudSyncIcon from "@mui/icons-material/CloudSync"
+import AddIcon from "@mui/icons-material/Add"
+
+interface AppbarProps {
+	setSidebarOpen: () => void
+};
+
+const Appbar = ({ setSidebarOpen }: AppbarProps) => {
+	return (
+		<AppBar position="fixed" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
+			<Toolbar>
+				<IconButton
+					size="large"
+					edge="start"
+					color="inherit"
+					aria-label="Main menu"
+					sx={{ mr: 2 }}
+					onClick={setSidebarOpen}
+				>
+					<MenuIcon />
+				</IconButton>
+
+				<Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+					Rasagi
+				</Typography>
+
+				<Box sx={{ display: { xs: 'none', md: 'flex' } }}>
+					<Tooltip title="Add source">
+						<IconButton
+							size="large"
+							aria-label="Add source to the feed"
+							color="inherit"
+						>
+							<AddIcon />
+						</IconButton>
+					</Tooltip>
+
+					<Tooltip title="Synchronization">
+						<IconButton
+							size="large"
+							color="inherit"
+							aria-label="Synchronization settings"
+						>
+							<CloudSyncIcon />
+						</IconButton>
+					</Tooltip>
+				</Box>
+			</Toolbar>
+		</AppBar>
+	)
+}
+
+export default Appbar;
---
+++ components/sidebar.tsx
@@ -0,0 +1,151 @@
+/**
+ * Sidebar containing complete feed, discovery screen and directories for feed sources.
+ *
+ * Currently, visibility state and selection action is controlled by props, maybe Redux will be useful here.
+ */
+
+import Box from '@mui/material/Box';
+import Drawer from "@mui/material/Drawer"
+import Toolbar from '@mui/material/Toolbar';
+import Typography from "@mui/material/Typography";
+import { SvgIconProps } from '@mui/material/SvgIcon';
+import TreeItem, { TreeItemProps, treeItemClasses } from '@mui/lab/TreeItem';
+import TreeView from "@mui/lab/TreeView";
+import { styled } from '@mui/material/styles';
+
+// Icons
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import ArrowRightIcon from '@mui/icons-material/ArrowRight';
+import Label from '@mui/icons-material/Label';
+import Newspaper from "@mui/icons-material/Newspaper";
+import Discover from "@mui/icons-material/Explore";
+
+declare module 'react' {
+	interface CSSProperties {
+		'--tree-view-color'?: string;
+		'--tree-view-bg-color'?: string;
+	}
+}
+
+type StyledTreeItemProps = TreeItemProps & {
+	bgColor?: string;
+	color?: string;
+	labelIcon: React.ElementType<SvgIconProps>;
+	labelInfo?: string;
+	labelText: string;
+}
+
+// Tree view item styles
+const StyledTreeItemRoot = styled(TreeItem)(({ theme }) => ({
+	color: theme.palette.text.secondary,
+	[`& .${treeItemClasses.content}`]: {
+		color: theme.palette.text.secondary,
+		borderTopRightRadius: theme.spacing(12),
+		borderBottomRightRadius: theme.spacing(12),
+		paddingRight: theme.spacing(1),
+		fontWeight: theme.typography.fontWeightMedium,
+		height: 42,
+		'&.Mui-expanded': {
+			fontWeight: theme.typography.fontWeightRegular,
+		},
+		'&:hover': {
+			backgroundColor: theme.palette.action.hover,
+		},
+		'&.Mui-focused, &.Mui-selected, &.Mui-selected.Mui-focused': {
+			backgroundColor: `var(--tree-view-bg-color, ${theme.palette.action.selected})`,
+			color: 'var(--tree-view-color)',
+		},
+		[`& .${treeItemClasses.label}`]: {
+			fontWeight: 'inherit',
+			color: 'inherit',
+		},
+	},
+	[`& .${treeItemClasses.group}`]: {
+		marginLeft: 0,
+		[`& .${treeItemClasses.content}`]: {
+			paddingLeft: theme.spacing(2),
+		},
+	},
+}))
+
+// Tree view item itself
+const StyledTreeItem = (props: StyledTreeItemProps) => {
+	const {
+		bgColor,
+		color,
+		labelIcon: LabelIcon,
+		labelInfo,
+		labelText,
+		...other
+	} = props;
+
+	return (
+		<StyledTreeItemRoot
+			label={
+				<Box sx={{ display: 'flex', alignItems: 'center', p: 0.5, pr: 0 }}>
+					<Box component={LabelIcon} color="inherit" sx={{ mr: 1 }} />
+					<Typography variant="body2" sx={{ fontWeight: 'inherit', flexGrow: 1 }}>
+						{labelText}
+					</Typography>
+					<Typography variant="caption" color="inherit">
+						{labelInfo}
+					</Typography>
+				</Box>
+			}
+			style={{
+				'--tree-view-color': color,
+				'--tree-view-bg-color': bgColor,
+			}}
+			{...other}
+		/>
+	);
+}
+
+interface SidebarProps {
+	isOpen: boolean,
+	onNodeSelect: (node: string) => void
+}
+
+const Sidebar = ({ isOpen, onNodeSelect }: SidebarProps) => {
+	const width = 260;
+
+	return (
+		<Drawer
+			variant="persistent"
+			sx={{
+				width: width,
+				flexShrink: 0,
+				[`& .MuiDrawer-paper`]: {
+					width: width,
+					boxSizing: 'border-box',
+					paddingTop: 2,
+					borderRight: 'none'
+				},
+			}}
+			open={isOpen}
+		>
+			<Toolbar />
+			<Box sx={{ overflow: 'auto' }}>
+				<TreeView
+					aria-label="Sidebar"
+					defaultExpanded={['directory-all']}
+					defaultSelected={'feed'}
+					defaultCollapseIcon={<ArrowDropDownIcon />}
+					defaultExpandIcon={<ArrowRightIcon />}
+					defaultEndIcon={<div style={{ width: 24 }} />}
+					sx={{ flexGrow: 1, maxWidth: width, overflowY: 'auto', userSelect: 'none' }}
+					onNodeSelect={(event: React.SyntheticEvent, nodeIds: string) => {
+						if (!nodeIds.startsWith("directory-")) { onNodeSelect(nodeIds) }
+					}}
+				>
+					<StyledTreeItem nodeId="feed" labelText="Feed" labelIcon={Newspaper} />
+					<StyledTreeItem nodeId="discover" labelText="Discover" labelIcon={Discover} />
+					<StyledTreeItem nodeId="directory-all" labelText="All" labelIcon={Label}>
+					</StyledTreeItem>
+				</TreeView>
+			</Box>
+		</Drawer>
+	)
+}
+
+export default Sidebar;
