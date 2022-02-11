2cf65947c8e63dd1b4d81a67037bbab24828ce59
Firstbober <firstbober@tutanota.com>
2022-02-11 19:25:19 +0100

    Move newscard container to another component.

---
+++ src/components/news.tsx
@@ -0,0 +1,18 @@
+import Box from '@mui/material/Box';
+
+import NewsCard from '../components/newscard';
+
+const News = () => {
+	return <Box sx={{ maxWidth: 800 }}>
+		<NewsCard
+			url="https://somenews.com"
+			headline="My news headline"
+			description="Something great happend! Check it our right now!"
+			source="SomeNews"
+			image="https://picsum.photos/200"
+			pubDate="2020-01-22"
+		/>
+	</Box>
+}
+
+export default News
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,7 +15,7 @@ import Toolbar from '@mui/material/Toolbar';
 // App components
 import Sidebar from '../components/sidebar';
 import Appbar from '../components/appbar';
-import NewsCard from '../components/newscard';
+import News from '../components/news';
 import { endpoints, fetcher, fetcherAuth } from '../app/api';
 import { useAppDispatch, useAppSelector } from '../app/hook';
 import { updateDirectories, updateSyncID } from '../app/features/syncstate';
@@ -127,17 +127,8 @@ const Home: NextPage = () => {
 					<Toolbar />
 
 					{/* TODO: Synchronize SourceItems  */}
+					<News />
 
-					<Box sx={{ maxWidth: 800 }}>
-						<NewsCard
-							url="https://somenews.com"
-							headline="My news headline"
-							description="Something great happend! Check it our right now!"
-							source="SomeNews"
-							image="https://picsum.photos/200"
-							pubDate="2020-01-22"
-						/>
-					</Box>
 				</Box>
 			</Box>
 		)
